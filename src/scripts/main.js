5d8c45f6a563606269905264806e0891fd9979a9
Rafael Rosa <rafagrosa@gmail.com>
2024-02-19 17:40:33 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+AOS.init();
+
+const eventDate = new Date("Feb 25, 2024 19:00:00");
+const timeStamp = eventDate.getTime();
+
+const hourCount = setInterval(function() {
+    const now = new Date();
+    const timeStampNow = now.getTime();
+
+    const distUntilEvent = timeStamp - timeStampNow;
+
+    const dayIntoMs = 1000 * 60 * 60 * 24;
+    const hourIntoMs = 1000 * 60 * 60;
+    const minuteIntoMs = 1000 * 60;
+    const secondsIntoMs = 1000;
+
+    const daysUntilEvent = Math.floor(distUntilEvent / dayIntoMs);
+    const hoursUntilEvent =  Math.floor((distUntilEvent % dayIntoMs) / hourIntoMs);
+    const minutesUntilEvent = Math.floor((distUntilEvent % hourIntoMs) / minuteIntoMs);
+    const secondsUntilEvent = Math.floor((distUntilEvent % minuteIntoMs) / secondsIntoMs);
+
+    document.getElementById('contador').innerHTML = `${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`;
+
+    if (distUntilEvent < 0) {
+        clearInterval(hourCount);
+        document.getElementById('contador').innerHTML = 'Evento expirado!';
+    }
+
+}, 1000);
